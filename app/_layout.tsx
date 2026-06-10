43e296d39c9e558e18b3ae3eeae89f73f064af74
Segni Dessalegn <segnides@gmail.com>
2026-06-10 12:12:35 +0300

    refactor: clean up whitespace and indentation in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,26 +3,24 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../lib/query-client";
 
 export default function Layout() {
-    return (
-        <QueryClientProvider client={queryClient}>            <Stack
-            screenOptions={{
-                headerTitleAlign: "center",
-            }}
-        >
-            <Stack.Screen
-                name="index"
-                options={{
-                    title: "Home",
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen
-                name="location/[id]"
-                options={{
-                    title: "City Details",
-                }}
-            />
-        </Stack>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerTitleAlign: "center" }}>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="location/[id]"
+          options={{
+            title: "City Details",
+          }}
+        />
+      </Stack>
+    </QueryClientProvider>
+  );
 }
